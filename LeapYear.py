79e76da4a6dbf56bdc0b3f90b0932bd734476789
Mohammed Shakib <50780268+iMohammadShakib@users.noreply.github.com>
2020-05-12 01:12:59 +0600

    Added a comment

--- LeapYear.py
+++ LeapYear.py
@@ -1,12 +1,11 @@
+# This program can check leap year, is the year "leap year" or not.
 def is_leap(year):
     leap = False
 
-"""
-Conditions:
+""" Conditions:
 1. The year can be evenly divided by 4, is a leap year, unless:
 2. The year can be evenly divided by 100, it is NOT a leap year, unless:
 3. The year is also evenly divisible by 400. Then it is a leap year.
-
 """    
     #logic here
     if year % 4 == 0 and (year % 400 == 0 or year % 100 != 0):
