13183046fc9814ffa9bb1b8d5124a4e861c7c181
Mohammed Shakib <50780268+iMohammadShakib@users.noreply.github.com>
2020-05-11 11:09:41 +0600

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,18 @@
+# HackerRabk 30 days of code solved
+# Day 1: Data types
+i = 4
+d = 4.0
+s = 'HackerRank '
+# input
+a = input()
+b = input()
+c = input() 
+# process
+f = int(i) + int(a)
+g = float(d) + float(b)
+h = str(s) + str(c)
+#output
+print(f)
+print(g)
+print(h)
+ # Shakib; 11 May, 2020; 11:06 AM
