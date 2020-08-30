359550d64f0f1c383e401ebd346dba4781636632
Mohammad Shakib <50780268+iMohammadShakib@users.noreply.github.com>
2020-08-30 22:04:04 +0600

    :recycle:

--- SquareRoot.py
+++
@@ -1,6 +0,0 @@
-num = input("Enter a number: ")
-# process
-squareRoot = int(num) ** 0.5
-#output
-print('The square root of ', str(num), ' is: ', str(squareRoot))
-
--- square.py
+++
@@ -1,6 +0,0 @@
-num = input("Enter a number: ")
-# process
-square = int(num) ** 2
-#output
-print('The square of ', str(num), ' is: ', str(square))
-
