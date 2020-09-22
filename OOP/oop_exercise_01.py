03b6bbf2cd3e99c053dbef800c7c3eee78930808
Shakib <code.shakb@gmail.com>
2020-09-22 22:00:32 +0600

    Update oop_exercise_01.py

--- OOP/oop_exercise_01.py
+++ OOP/oop_exercise_01.py
@@ -1,8 +1,14 @@
 class Student:
-    def __init__(self):
-        print('Student object created')
+    def __init__(self, name, id):
+        self.name = name # instance variable
+        self.id = id # instance variable
+        # print('Student object created')
 
 
+s1 = Student('Bob', 11)
+s2 = Student('Ross', 21)
 
-s1 = Student()
-print(s1)
+print(s1.name, s1.id)
+print(s2.name, s2.id)
+s2.id = 111 #updating s2.id 
+print(s2.name, s2.id) # printng after updateing
