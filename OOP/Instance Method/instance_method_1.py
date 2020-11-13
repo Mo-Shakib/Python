d64cbc331fb6a110447654c2063a03ea2aa5d19e
Shakib <code.shakb@gmail.com>
2020-11-14 00:27:42 +0600

    Create instance_method_1.py

---
+++ OOP/Instance Method/instance_method_1.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self, name, id):
+        self.name = name # instance variable
+        self.id = id # instance variable
+
+    def details(self): # instance method
+        print(f'Name: {self.name}, ID: {self.id}')
+
+
+s1 = Student('Shakib', 1)
+s2 = Student('Asit', 2)
+
+s1.details()
+s2.details()
